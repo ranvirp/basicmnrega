1e2348a459f9a5747360b45d83ecd1ba177d0db5
Ranvir Prasad <ranvir.prasad@gmail.com>
2015-06-25 10:13:41 +0530

    change in designation

---
+++ commands/ZendeskController.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\commands;
+
+use yii\console\Controller;
+
+use Zendesk\API\Client as ZendeskAPI;
+use Zendesk\API\Http;
+public class ZendeskController extends Controller
+{
+public function actionGenauth()
+{
+$subdomain = "grievancecell";       // Your Zendesk subdomain
+$username = "upmgnrega@gmail.com";         // Your Zendesk login
+$oAuthId = "mnrega_complaint_management_up";          // The value you entered into the OAuth 'Unique Identifier' field
+$oAuthSecret = "be6e394fd1f545d0c4e3fb815e5aa74536456c08fe637363f89a0a40ac2d0326";    // The OAuth secret given to you by Zendesk
+
+$client = new ZendeskAPI($subdomain, $username);
+if ($_REQUEST['code']) {
+    $response = Http::oauth($client, $_REQUEST['code'], $oAuthId, $oAuthSecret);
+    if (($client->getDebug()->lastResponseCode == 200) && ($response->access_token)) {
+        echo "<h1>Success!</h1>";
+        echo "<p>Your OAuth token is: " . $response->access_token . "</p>";
+        echo "<p>Use this code before any other API call:</p>";
+        echo "<code>&lt;?<br />\$client = new ZendeskAPI(\$subdomain, \$username);<br />\$client->setAuth('oauth_token', '" . $response->access_token . "');<br />?&gt;</code>";
+    } else {
+        echo "<h1>Error!</h1>";
+        echo "<p>We couldn't get an access token for you. Please check your credentials and try again.</p>";
+    }
+} else {
+    echo "<a href=\"https://" . $subdomain . ".zendesk.com/oauth/authorizations/new?response_type=code&redirect_uri=" . ($_SERVER['HTTPS'] ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . "&client_id=" . $oAuthId . "&scope=read%20write\">Click to request an OAuth token</a>";
+}
