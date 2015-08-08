623ab3ea5b144874e7f50a41ccf5b8ae647abdbf
Ranvir Prasad <ranvir.prasad@gmail.com>
2015-08-08 02:03:58 -0500

    a

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=basicmnrega',
-    'username' => 'mac',
-    'password' => 'mac',
-    'charset' => 'utf8',
-];
