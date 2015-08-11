879b64c980a77a80845a4b1cd9a6361033dcc70b
ranvirp <ranvirp@users.noreply.github.com>
2015-08-11 13:29:42 +0530

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=nregaup0_testmnrega',
-    'username' => 'nregaup0_nrega',
-    'password' => 'admin123',
-    'charset' => 'utf8',
-];
