8f4bfad6ecb59007bc84a7b4c5878995616556cf
ranvirp <ranvirp@users.noreply.github.com>
2015-09-10 15:38:32 +0530

    Update actionmarkdisposed.php

--- modules/complaint/views/marking/actionmarkdisposed.php
+++ modules/complaint/views/marking/actionmarkdisposed.php
@@ -1,1 +1,1 @@
- <?=\yii\helpers\Html::a($text,'#',['onclick'=>'populateHtml(\''.\yii\helpers\Url::to(['/complaint/marking/markdisposed?id='.$id.'&markingid='.$markingid]).'\',\'complaint-action-div\',\''.$id.'-action\');return false;'])?>
+ <?=\yii\helpers\Html::a($text,'#',['onclick'=>'populateHtml(\''.\yii\helpers\Url::to(['/complaint/marking/markdisposed?id='.$id.'&markingid='.$markingid]).'\',\'complaint-action-div\');return false;'])?>
