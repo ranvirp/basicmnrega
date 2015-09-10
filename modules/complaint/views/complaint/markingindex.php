ccc05710752d8b6bdfed09386e61355fdca3adc8
ranvirp <ranvirp@users.noreply.github.com>
2015-09-10 15:24:44 +0530

    Update markingindex.php

--- modules/complaint/views/complaint/markingindex.php
+++ modules/complaint/views/complaint/markingindex.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use app\modules\mnrega\models\Marking;
+use app\modules\users\models\DesignationType;
 /* @var $this yii\web\View */
 /* @var $searchModel app\modules\mnrega\models\MarkingSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
