25f9c11a12a13369c03ca7e7fc409e943370f514
Ranvir Prasad <ranvir.prasad@gmail.com>
2015-09-23 04:37:42 +0530

    m

--- modules/gpsphoto/views/photo/photolist1.php
+++ modules/gpsphoto/views/photo/photolist1.php
@@ -3,7 +3,7 @@
  $url="'/".Yii::getAlias('@web')."/jsons/"."'+$(this).val()+'.json'";
 			
 echo \yii\helpers\Html::dropDownList('district',$district,\yii\helpers\ArrayHelper::map(\app\modules\mnrega\models\District::find()->orderBy('name_en asc')->asArray()->all(),'code','name_en'),['prompt'=>'None','onchange'=>'populateDropdown("'.Yii::getAlias('@web')."/jsons/".'"+$(this).val()+".json","block")']);
-echo \yii\helpers\Html::dropDownList('block',$block,\yii\helpers\ArrayHelper::map(\app\modules\mnrega\models\Block::find()->orderBy('name_en desc')->where(['district'=>$district])->asArray()->all(),'code','name_en'),['id'=>'block']);
+echo \yii\helpers\Html::dropDownList('block',$block,\yii\helpers\ArrayHelper::map(\app\modules\mnrega\models\Block::find()->orderBy('name_en desc')->where(['district_code'=>$district])->asArray()->all(),'code','name_en'),['id'=>'block']);
 echo '<button type="submit" value="Submit">Submit</button>';
 echo '</form>';
 echo '<div id="portfoliowrap">';
